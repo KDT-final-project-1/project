b7391c625e32f601582514fb983e85f172feaab9
kimteayoon <kim@youtu.com>
2026-06-19 14:08:07 +0900

    김태윤/중간머지/20260619

--- src/main/java/com/pixcel/app/PixcelApplication.java
+++ src/main/java/com/pixcel/app/PixcelApplication.java
@@ -1,9 +1,12 @@
 package com.pixcel.app;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.pixcel.app.**.mapper")
 public class PixcelApplication {
 
 	public static void main(String[] args) {
