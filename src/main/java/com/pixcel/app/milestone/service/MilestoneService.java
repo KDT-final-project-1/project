20db60889438b4cd086b7f6add81c3cd6d9f248d
kimdeokmo <sewonn201@naver.com>
2026-06-19 14:04:29 +0900

    김덕모-마일스톤-20260619

--- src/main/java/com/pixcel/app/milestone/service/MilestoneService.java
+++ src/main/java/com/pixcel/app/milestone/service/MilestoneService.java
@@ -1,6 +1,6 @@
 package com.pixcel.app.milestone.service;
 
 public interface MilestoneService {
- String createMilestone(MilestoneCreateRequestDTO requestDTO);
- MilestoneDetailResponseDTO getMilestoneDetailResponseDTO(String milestoneId);
+ String createMilestone(MilestoneCreateRequestDTO requestDTO); //마일스톤 생성 인터페이스
+// MilestoneDetailResponseDTO getMilestoneDetailResponseDTO(String milestoneId); //조회 인터페이스
 }
